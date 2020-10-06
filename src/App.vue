31d17e0b16487bb157edb5c1ef1a781ad065ef40
Николай Колганов <42237828+Espanol-nikkol@users.noreply.github.com>
2020-10-06 12:24:23 +1000

    structure & navigation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <header id='header'>
+    <menu-nav></menu-nav>
+    <MenuSearch></MenuSearch>
+  </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import MenuNav from '@/components/MenuNav.vue';
+import MenuSearch from '@/components/MenuSearch.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MenuSearch,
+    MenuNav,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
+<style lang='scss'>
+#header {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/MenuSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'MenuSearch',
+};
+</script>
+
+<style scoped>
+
+</style>
